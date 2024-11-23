7f208cf578c0c856f96c635b533c680711f5e876
umesh sahu <umeshsahu32@gmail.com>
2024-11-23 16:42:08 +0530

    form update

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./SideBar.module.css";
+import axios from "axios";
 
 const SideBar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -33,21 +34,58 @@ const SideBar = () => {
 
     formData.append("access_key", "e7ebfe3b-4e06-4498-83d8-dd7fe07bbe4e");
 
-    const response = await fetch("https://api.web3forms.com/submit", {
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
-      body: formData,
-    });
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
 
-    const data = await response.json();
+    console.log("res", res);
 
-    if (data.success) {
+    if (res.success) {
       setResult("Form Submitted Successfully");
       event.target.reset();
     } else {
-      console.log("Error", data);
-      setResult(data.message);
+      console.log("Error", res);
+      setResult(res.message);
     }
   };
+
+  // const onSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const form = event.target;
+  //   console.log("btnClicked", form);
+  //   const formData = new FormData(form);
+  //   setResult("Sending....");
+
+  //   try {
+  //     const response = await fetch(
+  //       "https://formsubmit.co/ajax/aobsales.india@gmail.com",
+  //       {
+  //         method: "POST",
+  //         body: formData,
+  //       }
+  //     );
+
+  //     const data = await response.json();
+
+  //     console.log("data==>", data);
+
+  //     if (data.success) {
+  //       setResult("Form submitted successfully.");
+  //       form.reset();
+  //     }
+  //   } catch (error) {
+  //     console.log("Error:", error);
+  //     setResult("Something went wrong. Please Try Again");
+  //   }
+  // };
 
   return (
     <div
